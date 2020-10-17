859c7ae580fb2bb6a9d5ecb790a6bf8f0447ca86
tienlen <thangtienlen@gmail.com>
2020-10-17 16:44:48 +0700

    phep toan co ban

--- BT2/BT2/Source.cpp
+++ BT2/BT2/Source.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
-
-using  namespace std;
-
+using namespace std;
+int tong(int a, int b);
+int hieu(int a, int b);
+int tich(int a, int b);
+float thuong(int a, int b);
 int main()
 {
-	cout << "Hello";
-	system("pause");
+	int a, b;
+	char phepToan;
+	cout << "Nhap 2 so nguyen: ";
+	cin >> a >> b;
+	cout << "Tinh gi? (+,-,*,/) ";
+	cin >> ch;
+	switch (phepToan)
+	{
+	case '+':
+		cout<<"tong = "<<tong(a,b);
+		break;
+	case '-':
+		cout<<"hieu= "<<hieu(a,b);
+		break;
+	case '*':
+		cout<<"Tich = "<<tich(a,b);
+		break;
+	case '/':
+
+		break;
+	default:
+		cout << "Chon sai phep toan";
+		break;
+	}
 	return 0;
 }
