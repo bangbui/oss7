f74e46fd38c704b4392a144a3d7bc812cd48656a
tienlen <thangtienlen@gmail.com>
2020-10-17 16:51:05 +0700

    chuvi

--- BT2/BT2/Source.cpp
+++ BT2/BT2/Source.cpp
@@ -6,6 +6,27 @@ int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	//Chuc nang tinh cho vi
+	int hinh;
+	cout<<"Ban muon tinh chu vi hinh nao?"
+	cin>>hinh;
+	switch(hinh)
+	{
+		case 1:
+			int dai,rong;
+			cout<<"Nhap chieu dai";
+			cin>>dai;
+			cout<<"Nhap chieu rong";
+			cin>>rong;
+			cout<<"Chu vi HCN la: "<<(dai+rong)*2;
+			break;
+		case 2:
+		int canh;
+			cout<<"Nhap chieu canh";
+			cin>>canh;
+			cout<<"Chu vi hinh vuong la: "<<canh*4;
+			break;
+	}	
 	int a, b;
 	char phepToan;
 	cout << "Nhap 2 so nguyen: ";
